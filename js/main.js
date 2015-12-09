7a9c934f6e6194efec01e3275624b3e968ef319e
Sean Rivis <sean.rivis@gmail.com>
2015-12-09 17:34:53 -0500

    add JS and try to get the bg to change on buttonclick

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// var nav = document.getElementsByClassName("flex-container")[0];
+
+
+document.getElementById("myButton").onclick = function() {document.getElementById("BODY")[0].style.backgroundColor = "#00ff00";
+}
+
+// nav.addEventListener("click", nav.innerHTML = "Stop It!");
